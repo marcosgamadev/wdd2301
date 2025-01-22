6ae843ae06d3b30fc7ea6b8ee0cb68161a2dee15
marcosgamadev <marcosgama.dev@gmail.com>
2025-01-22 19:33:21 -0300

    page

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById('currentYear').textContent += new Date().getFullYear();
+
+    document.getElementById('lastModified').textContent += document.lastModified;
+});
